e55dde078f4970842b018a13bc92d40f18b450f6
Nicola Hastings <nicolahastings@gmail.com>
2021-06-24 15:48:16 -0400

    NH | header dev

---
+++ florida-makes.js
@@ -0,0 +1,19 @@
+function handleTopTextLinks() {
+    var container = $('#MPheader > div.row:first-child > .col-md-12'),
+        link = $('#MPAuxNav ul.level1 li:first-child a');
+
+    $(link).prependTo(container);
+    $(link).addClass('floridamakes-network');
+}
+
+function handleHeader() {
+    $('.mep-logo .HtmlContent').addClass('mep-logo').appendTo('#Logo > .col-md-12');
+    $('.search-bar-top, .header-social').wrapAll('<div class="col-md-12 search-and-social" />');
+    $('.search-and-social').appendTo('#Logo');
+    $('.search-bar-top .form-control').attr('placeholder', 'Keyword Search...');
+}
+
+$(function () {
+    handleTopTextLinks();
+    handleHeader();
+});
