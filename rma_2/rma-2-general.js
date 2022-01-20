99cffb73f321650319fbcb7ec937efbe4fcbd456
Nicola Hastings <nicolahastings@gmail.com>
2022-01-20 12:24:57 -0500

    NH | sponsors

--- rma_2/rma-2-general.js
+++ rma_2/rma-2-general.js
@@ -3,6 +3,11 @@ function handleSearch() {
     $('.search-bar-top .form-control, #searchColumn .form-control').attr('placeholder', 'Keyword Search...');
 }
 
+function handleSponsors() {
+    $('.sponsor').wrapAll('<div class="sponsors" />');
+}
+
 $(function () {
     handleSearch();
+    handleSponsors();
 });
