1fa953ea046f6fa4ca22571a638e438deda21bb0
Nicola Hastings <nicolahastings@gmail.com>
2022-03-01 16:00:22 -0500

    NH | RMA 1 theme dev | theme-wide stylesheet

--- rma_1/rma-1-general.js
+++ rma_1/rma-1-general.js
@@ -132,6 +132,46 @@ function handleFeaturedEvent() {
     });
 }
 
+function handleInteriorPageTitles() {
+
+    if ($('#MainCopy_ContentWrapper').hasClass('hero-h1')) {
+        $('#MPOuter').addClass('hero-h1');
+        $('#MainCopy_ContentWrapper').removeClass('hero-h1');
+    } else {
+        var headingImage = !!($('.header-bg').html()) ? $('.header-bg') : $('.default-header-bg');
+    
+        $('#PageTitleH1').wrap('<div class="page-heading" />');
+    
+        handleBgImage($(headingImage), $('.page-heading'));
+    }
+
+}
+
+function handleTestimonials() {
+    $('.testimonial').wrapAll('<div class="testimonial-slider slick-dotted" />');
+    $('.testimonial-slider').slick({
+        arrows: true,
+        dots: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-arrow prev-arrow"><i class="fm fm-chevron-left" /></button>',
+        nextArrow: '<button type="button" class="slick-arrow next-arrow"><i class="fm fm-chevron-right" /></button>',
+        responsive: [
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    $('.testimonial').each(function() {
+        var self = $(this);
+
+        handleLink(self);
+    });
+}
+
 $(function () {
     handleSearch();
     handleServices();
@@ -146,4 +186,6 @@ $(function () {
     handleFeaturedContent();
     handleAppending();
     handleFeaturedEvent();
+    handleInteriorPageTitles();
+    handleTestimonials();
 });
